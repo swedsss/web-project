6c81c438bb2e9c2ecd7c8843be4fe20bf77230c4
Swedsss <y170806@yandex.ru>
2022-04-22 00:33:30 +0500

    Добавлена возможность произвести выплату другому участнику

--- blueprints/money_bp.py
+++ blueprints/money_bp.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, request, redirect
 from flask_login import login_required, current_user
 from data import db_session
-from data.models import User, Event, Money
-from forms.money_forms import EditMoneyForm
+from data.models import User, Event, Money, Member
+from forms.money_forms import EditMoneyForm, PayForm
 from constants import *
 
 
@@ -48,3 +48,73 @@ def edit_money(event_id, user_id):
         session.commit()
         return redirect(f"/events/{event.id}")
     return render_template("money_form.html", **params)
+
+
+@blueprint.route('/money/set_pay/<int:event_id>', methods=['GET', 'POST'])
+@login_required
+def set_pay(event_id):
+    params = {
+        'app_name': APP_NAME,
+    }
+    session = db_session.create_session()
+    event = session.query(Event).get(event_id)
+    if not event:
+        params['error'] = f'Мероприятия с id {event_id} не существует'
+        return render_template("base.html", **params)
+    if current_user not in event.members:
+        params['error'] = f'Производить выплаты могут только участники этого мероприятия'
+        return render_template("base.html", **params)
+    params['title'] = "Оплата"
+    form = PayForm()
+    form.user_to_id.choices = [(user.id, user.get_full_name()) for user in event.members
+                            if user != current_user]
+    params['form'] = form
+    if form.validate_on_submit():
+        return redirect(f"/money/do_pay/{event_id}/{current_user.id}"
+                        f"/{form.user_to_id.data}/{form.pay_sum.data}")
+    return render_template("pay_form.html", **params)
+
+
+@blueprint.route('/money/do_pay/<int:event_id>/<int:user_from_id>'
+                 '/<int:user_to_id>/<float:pay_sum>')
+@login_required
+def do_pay(event_id, user_from_id, user_to_id, pay_sum):
+    params = {
+        'app_name': APP_NAME,
+    }
+    session = db_session.create_session()
+    event = session.query(Event).get(event_id)
+    if not event:
+        params['error'] = f'Мероприятия с id {event_id} не существует'
+        return render_template("base.html", **params)
+    for user_id in [user_from_id, user_to_id]:
+        member = session.query(Member).get((event_id, user_id))
+        if not member:
+            params['error'] = f'Пользователь с id {user_id} не является участником ' \
+                              f'мероприятия с id {event_id}'
+            return render_template("base.html", **params)
+    pay_sum = round(pay_sum, 2)
+    if pay_sum <= 0:
+        params['error'] = f'Сумма платежа не может быть отрицательной: {pay_sum}'
+        return render_template("base.html", **params)
+
+    money_from = session.query(Money).get((event_id, user_from_id))
+    if not money_from:
+        money_from = Money()
+        money_from.event_id = event_id
+        money_from.user_id = user_from_id
+        event.money_list.append(money_from)
+        session.merge(event)
+    money_to = session.query(Money).get((event_id, user_to_id))
+    if not money_to:
+        money_to = Money()
+        money_to.event_id = event_id
+        money_to.user_id = user_from_id
+        event.money_list.append(money_to)
+        session.merge(event)
+
+    money_from.cost += pay_sum
+    money_to.cost -= pay_sum
+    session.commit()
+    return redirect(f"/events/{event_id}")
+
